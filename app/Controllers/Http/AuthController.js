0c538cffcfa82f475099e0a82fd18ba713936f0c
0zz1cl13 <seniorcl13@gmail.com>
2020-01-07 11:14:27 -0400

    feat(authcontroller): adicionada a autenticacao de usuario

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -12,8 +12,12 @@ class AuthController {
         return user;
     }
 
-    async authenticate() {
-        
+    async authenticate({ request, auth }) {
+        const { email, password } = request.all();
+
+        const token = await auth.attempt(email, password);
+
+        return token;
     }
 }
 
